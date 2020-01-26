e1ebc5f7ce9b5bca0962a9f09d87bc0ddfc7ed97
Sai Charan <tsaicharan03@gmail.com>
2020-01-25 21:00:57 -0600

    tuned values

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -40,7 +40,7 @@ public final class Constants
 
     public static final double Z_ROT_DAMPENING = 0.75;
 
-    public static final double TURN_TOLERANCE =  4;
+    public static final double TURN_TOLERANCE =  2;
     
     /**
      * Diameter of the drive wheel in meters. AKA 6 inches.
---
+++ src/main/java/frc/robot/subsystems/Throttle.java
@@ -0,0 +1,41 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotContainer;
+
+public class Throttle extends SubsystemBase {
+ 
+	
+	private final WPI_TalonSRX wheel;
+
+
+    public Throttle(WPI_TalonSRX wheel) 
+    {
+        this.wheel = wheel;
+    }
+    
+
+    public void updateSpeed() 
+    {
+        double pos = throttlePos();
+
+        if(pos<-.2 || pos >.2)
+            wheel.set(pos);
+        else
+            wheel.set(0);
+    
+    }
+ 
+    public double throttlePos()
+    {
+        return RobotContainer.getJoy().getThrottle();
+    }
+
+    public WPI_TalonSRX getMotor()
+    {
+        return wheel;
+    }
+}
