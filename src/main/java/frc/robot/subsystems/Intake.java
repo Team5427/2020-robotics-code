0714a677010c9f5f15b8a9e6e0853bd3a48f7147
ujjainidas <43097770+ujjainidas@users.noreply.github.com>
2020-01-26 18:59:27 -0600

    cleaned up files

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -3,13 +3,16 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Intake extends SubsystemBase {
+public class Intake extends SubsystemBase 
+{
     private SpeedController intake;
 
-    public Intake(SpeedController intake) {
+    public Intake(SpeedController intake) 
+    {
     this.intake = intake;
-}
-    public void moveIntake(double Intake){
+    }
+    public void moveIntake(double Intake)
+    {
         intake.set(Intake);
     }
     public void stop()
